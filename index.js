cc480e5b9b33398d2439351f17c4930fed849eef
AshiePleb <38188828+AshiePleb@users.noreply.github.com>
2021-04-15 19:26:28 +0100

    Reverted all changes from the past few commits so the plugin can work again.

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
 const { Plugin } = require('powercord/entities');
 const { get } = require('powercord/http');
-const manifest = require('manifest.json');
 
 module.exports = class ChuckNorrisPlugin extends Plugin {
 
   startPlugin () {
     powercord.api.commands.registerCommand({
-      command: 'chucknorris',
+      command: 'ChuckNorris',
       description: 'Pulls a random joke from the Chuck Norris API and sends it in chat.',
       usage: '{c}',
-      executor: this.cn.bind()
-    });
-    powercord.api.commands.registerCommand({
-      command: 'cninfo',
-      description: 'Information about ChuckNorris plugin.',
-      usage: '{c}',
-      executor: this.info.bind()
+      executor: this.joke.bind()
     });
   }
 
     pluginWillUnload() {
-        powercord.api.commands.unregisterCommand('chucknorris');
-        powercord.api.commands.unregisterCommand('cninfo');
+        powercord.api.commands.unregisterCommand('ChuckNorris');
     }
 
-  async cn() {
+  async joke() {
     const data = await get(`https://api.chucknorris.io/jokes/random`);
     return {
       send: true,
       result: `${data.body.value}`
     };
   }
-  
-  async info() {
-    return {
-      send: false,
-      result: {
-        type: 'rich',
-        title: 'ChuckNorris-PowerCord Plugin Info',
-        description: `**__Name:_** ${manifest.name}\n**__Description:_** ${manifest.description}\n**__Version:_** ${manifest.version}\n**__Author:_** ${manifest.author}`,
-      }
-    };
-  }
-  
-};
-
